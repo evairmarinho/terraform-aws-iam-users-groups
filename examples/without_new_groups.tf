0a6a59b239e5e20af37dc398ab5a176e95743aa3
Evair Marinho <evairmarinho@hotmail.com>
2021-04-02 23:28:09 -0300

    Remotion of credential variables

--- examples/without_new_groups.tf
+++ examples/without_new_groups.tf
@@ -3,8 +3,6 @@ module "without_new_groups" {
 
   source = "evairmarinho/iam-users-groups/aws"
   region = "us-east-1"
-  access_key = "<MY_ACCESS_KEY>"
-  secret_key = "<MY_SECRET_KEY>"
   create_groups = false
   users = {
       "user1": {
