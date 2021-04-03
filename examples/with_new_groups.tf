48b1cdf864e064cc754cdef26d9ad9d2d5295da0
Evair Marinho <evairmarinho@hotmail.com>
2021-04-02 22:58:53 -0300

    Addition of module source

--- examples/with_new_groups.tf
+++ examples/with_new_groups.tf
@@ -1,6 +1,6 @@
 module "with_new_groups" {
 
-  source = "../../.."
+  source = "evairmarinho/iam-users-groups/aws"
   region = "us-east-1"
   access_key = "<MY_ACCESS_KEY>"
   secret_key = "<MY_SECRET_KEY>"
--- examples/without_new_groups.tf
+++ examples/without_new_groups.tf
@@ -1,7 +1,7 @@
   
 module "without_new_groups" {
 
-  source = "../../.."
+  source = "evairmarinho/iam-users-groups/aws"
   region = "us-east-1"
   access_key = "<MY_ACCESS_KEY>"
   secret_key = "<MY_SECRET_KEY>"
